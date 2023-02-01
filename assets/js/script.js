83172df58a627417010a10d8336bf896e11b7f7c
winglovecola@gmail.com <winglovecola@gmail.com>
2023-02-01 01:37:50 -0500

    added google map function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,15 +20,73 @@ var searchHistoryData = [];
 let searchedLocationData = {};
 
 
+
+
 //reset local storage for testing
 //localStorage.removeItem ("search-history");
 
 
+let googlePhotoDisplayLimit = 4; //control how many google image display in the page
+
 let input, options, autocomplete;
+let map, marker, infoWindow, userLocation;
+
+
 
 
 //initializing google place api
 function initGoogleAutocomplete() {
+
+  //set default coordinate for Toronto
+  userLocation = {
+    lat: 43.6532,
+    lng: 79.3832
+  };
+
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: userLocation.lat, lng: userLocation.lng },
+    zoom: 7,
+  });
+
+
+  infowindow = new google.maps.InfoWindow({
+    content: "",
+    ariaLabel: "",
+  });
+
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(userLocation.lat, userLocation.lng),
+    map: map,
+  });  
+
+
+  //set map to user location if available
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) 
+  {
+    navigator.geolocation.getCurrentPosition((position) => {
+        
+      userLocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      map.setCenter(userLocation);
+      marker.setPosition(userLocation);
+
+    },
+    () => {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+
+
 
 
   //option for search cities in United States and Canada must include the following option
@@ -39,17 +97,37 @@ function initGoogleAutocomplete() {
   //  types: ['(cities)']
    // 
   // };
-  options = {
 
-   };
+  
+  options = {
+  };
 
   input = document.getElementById('search-input');
 
 
   autocomplete = new google.maps.places.Autocomplete(input, options);
     google.maps.event.addListener(autocomplete, 'place_changed', function () {
+      
       let place = autocomplete.getPlace();
       //console.log (place)
+
+
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+
+
+      marker.setVisible(false);
+      
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+
+      infowindow.open(map, marker);
+
 
       if (place.geometry)
       {
@@ -58,7 +136,7 @@ function initGoogleAutocomplete() {
         searchedLat = place.geometry.location.lat();
         searchedLng = place.geometry.location.lng();
 
-
+        //console.log (place);
 
         let adrAddress = place.adr_address; 
         
@@ -67,7 +145,9 @@ function initGoogleAutocomplete() {
         searchedLocationData.countryName = strExtract (adrAddress, "class=\"country-name\">", "</span>");
         searchedLocationData.lat = searchedLat;
         searchedLocationData.lng = searchedLng;
+        searchedLocationData.placeId = place.place_id;
 
+        googlePhotoApi (place, googlePhotoDisplayLimit, "#place-photo");
 
 
         //console.log ("searchedLocationData", searchedLocationData);
@@ -83,6 +163,42 @@ function initGoogleAutocomplete() {
 
 
 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+
+function googlePhotoApi (place, imageLimit, elementID) {
+
+  if (place)
+  {
+    if (!place.photos) {
+      return;
+    }
+
+    let photos = place.photos;
+    let photoUrl = "";
+    let placePhotoHtml = "";
+
+    if (photos.length < imageLimit)
+      imageLimit = photos.length;
+
+    for (let i = 0; i < imageLimit; i++)
+    {
+      photoUrl = photos[i].getUrl({maxWidth: 300, maxHeight: 300});
+      placePhotoHtml += "<div class='place-photo'><img src='" + photoUrl + "'></div>";
+    }
+    
+    
+    $(elementID).html (placePhotoHtml);
+  }
+}
 
 
 //add key listener for ENTER key
@@ -117,10 +233,29 @@ function geocodeSearch(placeName, placeAddress) {
   geocoder.geocode({"address":searchTerm}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) 
       {
-        //console.log (results[0]);
+        console.log ("Geocoder", results);
+        
+        place = results[0];
+          
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+        } else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(17);
+        }
+
+
+        marker.setVisible(false);
+        
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        infowindow.open(map, marker);
+        
         searchedLocation = placeName;
-        searchedLat = results[0].geometry.location.lat();
-        searchedLng = results[0].geometry.location.lng();
+        searchedLat = place.geometry.location.lat();
+        searchedLng = place.geometry.location.lng();
   
 
         //console.log (placeAddress)
@@ -129,13 +264,31 @@ function geocodeSearch(placeName, placeAddress) {
         searchedLocationData.countryName = placeAddress.substring (placeAddress.indexOf (",") + 1).trim();
         searchedLocationData.lat = searchedLat;
         searchedLocationData.lng = searchedLng;
-  
+        searchedLocationData.placeId = results[0].place_id;
+
+
+ 
+        placesService = new google.maps.places.PlacesService(map);
+     
+        placesService.getDetails(
+        {
+          placeId: place.place_id,
+          fields: ['name', 'photos']
+        },
+        (placeDetail) => {
+          //console.log (place);
+          googlePhotoApi (placeDetail, googlePhotoDisplayLimit, "#place-photo");
+
+        });
+
+
 
         //remove focus on search input
         input.blur();
 
         saveSearch (searchedLocationData);
-  
+
+        
         vacationCheckInfo (searchedLocationData);
       }
   });
@@ -153,17 +306,21 @@ function renderSearchedLocation () {
 
 
     //console.log ("searchHistoryDataRaw", searchHistoryData.length)
-    searchHistoryData.forEach (function (thisWeatherData) {
 
-      
+    let sdataId = 0;
+
+    
+    searchHistoryData.forEach (function (thisWeatherData) {
 
       let sdata = thisWeatherData.location + " " + thisWeatherData.region + ", " +  thisWeatherData.countryName;
 
-      weatherSearchHistoryHtml += "<div class='weather-history-tab' location='" + thisWeatherData.location + "' region='" + thisWeatherData.region + "' countryName='" + thisWeatherData.countryName + "' lat='" + thisWeatherData.lat + "' lng='" + thisWeatherData.lng + "' sdata='" + sdata + "'><b>" + thisWeatherData.location + "</b> " + thisWeatherData.region + ", " +  thisWeatherData.countryName + " </div>";
+      weatherSearchHistoryHtml += "<div class='weather-history-tab' sdata-id='" + sdataId + "' location='" + thisWeatherData.location + "' region='" + thisWeatherData.region + "' countryName='" + thisWeatherData.countryName + "' lat='" + thisWeatherData.lat + "' lng='" + thisWeatherData.lng + "' sdata='" + sdata + "'><b>" + thisWeatherData.location + "</b> " + thisWeatherData.region + ", " +  thisWeatherData.countryName + " </div>";
+    
+      sdataId++;
     });
     
 
-    $('#weather-search-history').html ("<br><br><br><br>Search History:<br><br>" + weatherSearchHistoryHtml);
+    $('#weather-search-history').html ("<br><br><br><br>Search History:<br><br>" + weatherSearchHistoryHtml + "<div id='clear-history-div'><div onclick='clearHistory ()'>Clear History</div></div>");
 
 
     $( ".weather-history-tab" ).on("click", function() {
@@ -171,21 +328,55 @@ function renderSearchedLocation () {
       //set search bar text
       document.getElementById('search-input').value = $(this).attr ("sdata");
     
-      searchedLocation = $(this).attr ("location");
+ 
+      thisSdataId = $(this).attr ("sdata-id");
+      searchedLocationData = searchHistoryData[thisSdataId];
+      
+      searchedLocation = searchedLocationData.location;
+      
 
 
-      searchedLocationData.location = searchedLocation;
-      searchedLocationData.region = $(this).attr ("region");
-      searchedLocationData.countryName = $(this).attr ("countryName");
-      searchedLocationData.lat = $(this).attr ("lat");
-      searchedLocationData.lng = $(this).attr ("lng");
+      placesService = new google.maps.places.PlacesService(map);
+     
 
-      //check weather api
+      placesService.getDetails
+      (
+        {
+          placeId: searchedLocationData.placeId,
+          fields: ['name', 'photos', 'geometry']
+        },
+        (place) => {
+          console.log (place);
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+          }
+
+
+          marker.setVisible(false);
+          
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          infowindow.open(map, marker);
+
+          googlePhotoApi (place, googlePhotoDisplayLimit, "#place-photo");
+        }
+      );
+
+
+      //console.log (searchedLocationData)
+
+
+      //check info, to do, and weather
       vacationCheckInfo (searchedLocationData);
 
     });
   }
-
 }
 
 
@@ -230,11 +421,10 @@ function vacationCheckInfo (thisSearchedLocationData) {
   //ChatGPT Api
   let askLocation = thisSearchedLocationData.location + ", " + thisSearchedLocationData.countryName;
   chatGptApi ("describe a vacation to \"" + askLocation + "\"", "#place-info");
+  
+  
   chatGptApi ("things to do in \"" + askLocation + "\"", "#place-todo");
   
-
-
-
 
   //open weather api 
   let openWeatherApiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + thisSearchedLocationData.lat + "&lon=" + thisSearchedLocationData.lng + "&appid=55c6ad05fb90696b0befe8a67cb935d7";
@@ -289,9 +479,6 @@ function weatherShow (weatherDataRaw) {
 
   weatherDataRaw.list.forEach (function (thisWeather) {
 
-
-
-    
     let todayDate = dayjs ();
     let weatherDate = dayjs(thisWeather.dt * 1000);
 
@@ -463,26 +650,23 @@ function saveSearch (thisSearchedLocationData) {
   renderSearchedLocation ();
 }
 
-/*
-curl https://api.openai.com/v1/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d '{
-  "model": "text-davinci-003",
-  "prompt": "disney world\n\nDisney World is a theme park resort complex in Lake Buena Vista, Florida, near Orlando. It is owned and operated by The Walt Disney Company through its Parks, Experiences and Products division. The resort opened on October 1, 1971, and is the most visited vacation resort in the world, with an attendance of over 66 million people in 2019. The resort is composed of four theme parks (Magic Kingdom, Epcot, Disney's Hollywood Studios, and Disney’s Animal Kingdom), two water parks (Disney's Typhoon Lagoon and Disney's Blizzard Beach), a shopping, dining, and entertainment complex (Disney Springs), 27 themed resort hotels, nine non-Disney hotels, several golf courses, a camping resort, and other entertainment venues.",
-  "temperature": 0.7,
-  "max_tokens": 256,
-  "top_p": 1,
-  "frequency_penalty": 0,
-  "presence_penalty": 0
-}'
-*/
+
+
+function clearHistory () {
+
+  localStorage.removeItem ("search-history");
+  searchHistoryData = [];
+  $('#weather-search-history').html ("");
+  
+}
+
+
+
 
 //chatGPT
-
-
 function chatGptApi(search, elementID) {
  
+
   let chatGptApiUrl = "https://api.openai.com/v1/completions";
   let chatGptApiKey = "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";
   
@@ -501,23 +685,32 @@ function chatGptApi(search, elementID) {
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + chatGptApiKey
-  
     },
     body: JSON.stringify(fetchData)
-    
   };
 
+
+  let loadText = "";
+  
   //loading icon
   if ($(elementID))
-    $(elementID).html ('<div id="loading-div"><img id="loading-icon" src="./assets/images/loading2.gif"></div>');
-
+  {
+    if (elementID == "#place-info")
+      loadText = " Looking into this place for you.";
+    else if (elementID == "#place-todo")
+      loadText = " Finding fun things to do, please wait.";
+    else
+      loadText = "";
+    
+    $(elementID).html ('<div id="loading-div"><img id="loading-icon" src="./assets/images/loading2.gif">' + loadText + '</div>');
+  }
 
   fetch(chatGptApiUrl, fetchOptions)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-      console.log("data: ", data);
+    //console.log("data: ", data);
 
     //loading data into page
     if ($(elementID))
@@ -563,23 +756,14 @@ function chatGptApi(search, elementID) {
   });
 }
 
-
+//create moving effect on the header bar
 const el = document.querySelector("#header-div");
 
 el.addEventListener("mousemove", (e) => {
-  el.style.backgroundPositionX = (-e.offsetX * 0.20) + "px";
-  //el.style.backgroundPositionY = -e.offsetY + "px";
+  if (e.target.id != "search-question")
+    el.style.backgroundPositionX = (-e.offsetX * 0.20) + "px";
+
 });
-el.addEventListener("mouseout", (e) => {
-  //el.style.backgroundPositionX = "300px";
-  //el.style.backgroundPositionY = "380px";
-});
-
-
-//chatGptApi ("describe new york, united state vacation", "#place-info");
-//chatGptApi ("things to do in disney world", "#place-todo");
-
-
 
 
 
